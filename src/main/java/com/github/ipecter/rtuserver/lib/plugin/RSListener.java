a88aa45c2b23349ca46ae0f768615c2d2f38b656
ipecter <ipectert@gmail.com>
2024-03-08 11:37:15 +0900

    Fix

--- src/main/java/com/github/ipecter/rtuserver/lib/plugin/RSListener.java
+++ src/main/java/com/github/ipecter/rtuserver/lib/plugin/RSListener.java
@@ -1,18 +1,9 @@
 package com.github.ipecter.rtuserver.lib.plugin;
 
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 public abstract class RSListener implements Listener {
     @Getter
     private final RSPlugin plugin = RSPlugin.getPlugin();
-
-    public RSListener() {
-        this(true);
-    }
-
-    public RSListener(boolean isEnable) {
-        if (isEnable) Bukkit.getPluginManager().registerEvents(this, plugin);
-    }
 }
